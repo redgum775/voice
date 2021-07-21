ec71e2481a81e7cc87154f3b4bbb070472faf115
redgum <redgum775@gmail.com>
2021-07-21 17:19:54 +0900

    Add connection message.

--- src/client.py
+++ src/client.py
@@ -3,8 +3,10 @@ import sys, time
 
 
 def main():
-    target_host = '127.0.0.1'
-    target_port = 5001
+    # target_host = '127.0.0.1'
+    target_host = '192.168.1.7'
+    # target_host = '192.168.1.24'
+    target_port = 5010
 
     try:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,6 +17,7 @@ def main():
 
     try:
         client.connect((target_host, target_port))
+        print("Connection to server")
     except socket.error:
         print('Could not connect to server')
         time.sleep(1)
